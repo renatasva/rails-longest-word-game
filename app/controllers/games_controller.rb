ef43e1fafebb7760253894030a41c5c4b246584d
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2018-11-06 11:30:12 +0000

    Longest Word Game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters].split
+    @included = included?(@word, @letters)
+    @is_english = english_word?(@word)
+    # english_word?(params[:word]) && included?(params[:word], @letters)
+  end
+
+  private
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+end
+
